e93407bfc6856bcf97e8e2d659bcf72d1f6320cd
clear <limpid666@163.com>
2022-08-16 01:04:47 +0800

    底部tabBar的完善与配置

---
+++ App.vue
@@ -0,0 +1,24 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	@import url("./common/free.css");
+	/* 主要的背景颜色 */
+	@import url("./common/common.css");
+	/* 动画效果 */
+	@import url("./common/animate.min.css");
+	/* 引入图标库 */
+	@import url("./common/iconfont.css");
+</style>
---
+++ pages/tabber/index/index.vue
@@ -0,0 +1,14 @@
+<template>
+	<view class="container">
+		<view class="flex align-center justify-center bg-danger">
+			<text class="text-white py-3">123</text>
+		</view>
+	</view>
+</template>
+
+<script>
+
+</script>
+
+<style>
+</style>
